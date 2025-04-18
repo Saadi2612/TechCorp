da3dc8eaba3444b3a7608dd0e543128fb4757cc9
Muhammad Saad Hasan <saadhassan817@gmail.com>
2025-04-18 17:32:40 +0500

    removed unused variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, DM_Sans } from "next/font/google";
+import { Inter } from "next/font/google";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import "./globals.css";
